219f8f14b131c564a427aa3a6007d0ce535dd14d
sunny5techmaster@gmail.com <sunny5techmaster@gmail.com>
2021-07-01 14:20:49 +0530

    Updated main.py and added requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+# Strong Password Generator
+
+# Importing Modules Required
 import string
 import random
+import pyfiglet as pf
 
+# Banner
+banner = pf.figlet_format("Password Generator")
+print(banner)
+
+# Main Code
 if __name__ == "__main__":
 
     s1 = string.ascii_lowercase
-    # print(s1)
     s2 = string.ascii_uppercase
-    # print(s2)
     s3 = string.digits
-    # print(s3)
     s4 = string.punctuation
-    # print(s4)
 
     plen = int(input("Enter the Length of Password : "))
 
@@ -19,7 +24,6 @@ if __name__ == "__main__":
     s.extend(list(s2))
     s.extend(list(s3))
     s.extend(list(s4))
-    # print(s)
     random.shuffle(s)
-    # print(s)
+    print("Your Password is :")
     print("".join(s[0:plen]))
